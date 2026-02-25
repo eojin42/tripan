b14ea49e24e0aaa30c42339612201273808e528e
unknown <eojinkim42@gmail.com>
2026-02-25 17:30:21 +0900

    First commit

---
+++ src/main/java/com/tripan/app/TripanApplication.java
@@ -0,0 +1,13 @@
+package com.tripan.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tripan/app/TripanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tripan.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
