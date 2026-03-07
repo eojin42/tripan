16a417dc00257e6a6d184a6233a7220954bcda43
nowk01 <rnjsgurcks5432@gmail.com>
2026-03-08 06:36:28 +0900

    시큐리티 수정

--- src/main/java/com/tripan/app/config/SpringSecurityConfig.java
+++ src/main/java/com/tripan/app/config/SpringSecurityConfig.java
@@ -27,7 +27,7 @@ public class SpringSecurityConfig {
 		String[] excludeUri = {"/", "/index.jsp", "/member/login", "/member/account", "/member/logout",
 				"/member/userIdCheck", "/member/complete", "/member/pwdFind", "/member/expired", "/dist/**",
 				"/guest/main", "/guest/list", "/uploads/photo/**", "/favicon.ico", "/WEB-INF/views/**",
-				"/oauth/kakao/callback", "/accommodation/home", "/accommodation/list", 
+				"/oauth/kakao/callback", "/accommodation/home", "/accommodation/list", "/accommodation/search",
 				"/community/feed", "/community/fragment/feed", "/api/festivals/**", "/community/api/chat/top-rooms",
 				"/ws-tripan/**"};
 		
