5261cbce7e7736d0db4a5a1c10d4f59061b09c02
user <eojinkim42@gmail.com>
2026-03-17 17:48:17 +0900

    집갈준비

--- src/main/java/com/tripan/app/service/MemberCouponServiceImpl.java
+++ src/main/java/com/tripan/app/service/MemberCouponServiceImpl.java
@@ -2,7 +2,9 @@ package com.tripan.app.service;
 
 import java.sql.SQLException;
 import java.time.LocalDateTime;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.stereotype.Service;
 
@@ -14,6 +16,7 @@ import com.tripan.app.domain.entity.MemberCoupon;
 import com.tripan.app.mapper.MemberCouponMapper;
 import com.tripan.app.repository.MemberCouponRepository;
 
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -26,6 +29,7 @@ public class MemberCouponServiceImpl implements MemberCouponService{
 	private final MemberCouponRepository memberCouponRepository;
 	
 	@Override
+	@Transactional
 	public void issueWelcomeCoupon(Member1 member1) throws Exception {
 		 // NEW_MEMBER 조건이고 현재 유효한 쿠폰 목록 조회
         List<CouponDto.ListItem> welcomeCoupons =
@@ -35,14 +39,12 @@ public class MemberCouponServiceImpl implements MemberCouponService{
             log.info("발급할 신규가입 쿠폰 없음 (member_id={})", member1.getId());
             return;
         }
- 
-        // 각 쿠폰을 member_coupon 에 INSERT
+        
         for (CouponDto.ListItem coupon : welcomeCoupons) {
             MemberCoupon memberCoupon = new MemberCoupon();
             memberCoupon.setMemberId(member1.getId());
             memberCoupon.setCouponId(coupon.getCouponId());
             memberCoupon.setExpiredAt(LocalDateTime.now().plusDays(7));
-            // status, issuedAt 은 엔티티/DB 기본값으로 처리됨
  
             memberCouponRepository.save(memberCoupon);
             log.info("신규가입 쿠폰 발급 완료 member_id={}, coupon_id={}",
@@ -54,5 +56,26 @@ public class MemberCouponServiceImpl implements MemberCouponService{
 	public List<MemberCouponDto> getMyCouponList(Long memberId, String status) throws SQLException {
 		
 		return memberCouponMapper.getMyCouponList(memberId, status);
+	}
+
+	@Transactional
+	@Override
+	public void insertCoupon(Member1 member1) throws Exception {
+		 List<CouponDto.ListItem> coupons = couponMapper.findByConditionType("NONE");
+		 Map<String, Object> params = new HashMap<>();
+		 params.put("conditionType", "NONE");
+		 List<Long> allMemberIds = couponMapper.findMemberIdsByCondition(params);
+		 
+		 for (Long memberId : allMemberIds) {
+		        for (CouponDto.ListItem coupon : coupons) {
+		            MemberCoupon memberCoupon = new MemberCoupon();
+		            memberCoupon.setMemberId(memberId);
+		            memberCoupon.setCouponId(coupon.getCouponId());
+		            memberCoupon.setExpiredAt(coupon.getValidUntil());
+		            
+		            memberCouponRepository.save(memberCoupon);
+		        }
+		      log.info("회원 ID {}에게 쿠폰 {}종 발급 완료", memberId, coupons.size());
+		    }
 	}
 }
